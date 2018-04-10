29a4a2207c7f5a0b2d11884eba457b6d85b28b89
rurigrass <ruripg@gmail.com>
2018-04-10 16:21:56 +0100

    finish?

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -29,3 +29,7 @@ end
 def add_string_as_number(string_1, string_2)
   return string_1.to_i + string_2.to_i
 end
+
+def number_to_full_month_name(month_1, month_3, month_9)
+  return month_1.to_s month_3.to_s month_9.to_s
+end
