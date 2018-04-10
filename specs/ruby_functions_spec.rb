512364a0f9776de864a67943e76cba48fe36c611
rurigrass <ruripg@gmail.com>
2018-04-10 15:48:19 +0100

    test 5

--- specs/ruby_functions_spec.rb
+++ specs/ruby_functions_spec.rb
@@ -22,12 +22,12 @@ class Functions_Practice < MiniTest::Test
     multiply_result = multiply( 4, 2 )
     assert_equal( 8, multiply_result )
   end
+
+  def test_divide()
+    divide_result = divide( 10, 2 )
+    assert_equal( 5, divide_result )
+  end
   
-  # def test_divide()
-  #   divide_result = divide( 10, 2 )
-  #   assert_equal( 5, divide_result )
-  # end
-  #
   # def test_length_of_string()
   #   test_string = "A string of length 21"
   #   length_of_string = length_of_string( test_string )
