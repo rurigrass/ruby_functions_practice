2db63c71db42d4871f30e3843ead41754f5f9370
rurigrass <ruripg@gmail.com>
2018-04-10 15:47:18 +0100

    test 3

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -9,3 +9,7 @@ end
 def subtract(num1, num2)
   return num1 - num2
 end
+
+def multiply(num1, num2)
+  return num1 * num2
+end
--- specs/ruby_functions_spec.rb
+++ specs/ruby_functions_spec.rb
@@ -17,12 +17,12 @@ class Functions_Practice < MiniTest::Test
     subtract_result = subtract( 10, 5 )
     assert_equal( 5, subtract_result )
   end
+
+  def test_multiply()
+    multiply_result = multiply( 4, 2 )
+    assert_equal( 8, multiply_result )
+  end
   
-  # def test_multiply()
-  #   multiply_result = multiply( 4, 2 )
-  #   assert_equal( 8, multiply_result )
-  # end
-  #
   # def test_divide()
   #   divide_result = divide( 10, 2 )
   #   assert_equal( 5, divide_result )
